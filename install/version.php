dd397c8ad57c675b88ee15f3ccbf04c0e5b260ed
pavel <rover.webdev@gmail.com>
2016-12-07 00:03:39 +0500

    bx 1.0.0 version

--- install/version.php
+++ install/version.php
@@ -1,5 +1,5 @@
 <?php
-$arModuleVersion = [
+$arModuleVersion = array(
     "VERSION"			    => "1.0.0",
-    "VERSION_DATE"		    => "2016-12-06 00:00:00",
-];
+    "VERSION_DATE"		    => "2016-12-06 23:47:00",
+);
